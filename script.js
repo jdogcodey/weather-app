7f759d2be10744c2b9334e8a78e7cdf192a766f0
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-05-30 17:09:03 +0100

    Update all inner HTML with data

--- script.js
+++ script.js
@@ -75,26 +75,107 @@ function updateImageSrc(element, content) {
 
 async function updatePage(location) {
   try {
-    let locationObject = await makeApiRequest(`${location}`);
-    updateImageSrc(
-      document.getElementById("current-tile"),
-      `https:${locationObject.current.image}`
-    );
-    updateImageSrc(
-      document.getElementById("tile-1"),
-      `https:${locationObject.dayOne.image}`
-    );
-    updateImageSrc(
-      document.getElementById("tile-2"),
-      `https:${locationObject.dayTwo.image}`
-    );
-    updateImageSrc(
-      document.getElementById("tile-3"),
-      `https:${locationObject.dayThree.image}`
-    );
+    updateBackgrounds(location);
+    updateAllFields(location);
   } catch (error) {
     console.log(error);
   }
 }
 
-updatePage("lisbon");
+async function updateBackgrounds(location) {
+  let locationObject = await makeApiRequest(`${location}`);
+  let currentTile = document.getElementById("current-tile");
+  let tile1 = document.getElementById("tile-1");
+  let tile2 = document.getElementById("tile-2");
+  let tile3 = document.getElementById("tile-3");
+  updateImageSrc(currentTile, `https:${locationObject.current.image}`);
+  updateImageSrc(tile1, `https:${locationObject.dayOne.image}`);
+  updateImageSrc(tile2, `https:${locationObject.dayTwo.image}`);
+  updateImageSrc(tile3, `https:${locationObject.dayThree.image}`);
+}
+
+async function updateAllFields(location) {
+  let locationObject = await makeApiRequest(location);
+  let currentTemp = document.getElementById("current-temp-number");
+  let currentFeelsLike = document.getElementById("current-temp-feels-like");
+  let currentWindNumber = document.getElementById("current-wind-number");
+  let currentWindDirection = document.getElementById("current-wind-direction");
+  let currentRainNumber = document.getElementById("current-rain-number");
+  let currentHumidityNumber = document.getElementById(
+    "current-humidity-number"
+  );
+  let currentLocation = document.getElementById("current-location");
+  let currentRegion = document.getElementById("current-region");
+  let currentComment = document.getElementById("current-comment");
+  let dateOne = document.getElementById("1-date");
+  let tempOne = document.getElementById("1-temp");
+  let windOne = document.getElementById("1-wind");
+  let rainPercOne = document.getElementById("1-rain-perc");
+  let rainChanceOne = document.getElementById("1-rain-chance");
+  let commentOne = document.getElementById("1-comment");
+  let daylightOne = document.getElementById("1-daylight");
+  let dateTwo = document.getElementById("2-date");
+  let tempTwo = document.getElementById("2-temp");
+  let windTwo = document.getElementById("2-wind");
+  let rainPercTwo = document.getElementById("2-rain-perc");
+  let rainChanceTwo = document.getElementById("2-rain-chance");
+  let commentTwo = document.getElementById("2-comment");
+  let daylightTwo = document.getElementById("2-daylight");
+  let dateThree = document.getElementById("3-date");
+  let tempThree = document.getElementById("3-temp");
+  let windThree = document.getElementById("3-wind");
+  let rainPercThree = document.getElementById("3-rain-perc");
+  let rainChanceThree = document.getElementById("3-rain-chance");
+  let commentThree = document.getElementById("3-comment");
+  let daylightThree = document.getElementById("3-daylight");
+  updateInnerHTML(currentTemp, `${locationObject.current.temp}°`);
+  updateInnerHTML(currentFeelsLike, `${locationObject.current.feelslike}°`);
+  updateInnerHTML(currentWindNumber, `${locationObject.current.wind}mph`);
+  updateInnerHTML(currentWindDirection, locationObject.current.windDir);
+  updateInnerHTML(currentRainNumber, `${locationObject.current.precip}mm`);
+  updateInnerHTML(currentHumidityNumber, `${locationObject.current.humidity}%`);
+  updateInnerHTML(currentLocation, locationObject.location);
+  updateInnerHTML(currentRegion, locationObject.region);
+  updateInnerHTML(currentComment, locationObject.current.comment);
+  updateInnerHTML(dateOne, locationObject.dayOne.date);
+  updateInnerHTML(
+    tempOne,
+    `${locationObject.dayOne.minTemp}° - ${locationObject.dayOne.maxTemp}°`
+  );
+  updateInnerHTML(windOne, `${locationObject.dayOne.maxWind}mph`);
+  updateInnerHTML(rainPercOne, `${locationObject.dayOne.willRain}%`);
+  updateInnerHTML(rainChanceOne, `${locationObject.dayOne.willRain}%`);
+  updateInnerHTML(commentOne, locationObject.dayOne.comment);
+  updateInnerHTML(
+    daylightOne,
+    `${locationObject.dayOne.sunrise} - ${locationObject.dayOne.sunset}`
+  );
+  updateInnerHTML(dateTwo, locationObject.dayTwo.date);
+  updateInnerHTML(
+    tempTwo,
+    `${locationObject.dayTwo.minTemp}° - ${locationObject.dayTwo.maxTemp}°`
+  );
+  updateInnerHTML(windTwo, `${locationObject.dayTwo.maxWind}mph`);
+  updateInnerHTML(rainPercTwo, `${locationObject.dayTwo.willRain}%`);
+  updateInnerHTML(rainChanceTwo, `${locationObject.dayTwo.willRain}%`);
+  updateInnerHTML(commentTwo, locationObject.dayTwo.comment);
+  updateInnerHTML(
+    daylightTwo,
+    `${locationObject.dayTwo.sunrise} - ${locationObject.dayTwo.sunset}`
+  );
+  updateInnerHTML(dateThree, locationObject.dayThree.date);
+  updateInnerHTML(
+    tempThree,
+    `${locationObject.dayThree.minTemp}° - ${locationObject.dayThree.maxTemp}°`
+  );
+  updateInnerHTML(windThree, `${locationObject.dayThree.maxWind}mph`);
+  updateInnerHTML(rainPercThree, `${locationObject.dayThree.willRain}%`);
+  updateInnerHTML(rainChanceThree, `${locationObject.dayThree.willRain}%`);
+  updateInnerHTML(commentThree, locationObject.dayThree.comment);
+  updateInnerHTML(
+    daylightThree,
+    `${locationObject.dayThree.sunrise} - ${locationObject.dayThree.sunset}`
+  );
+}
+
+updatePage("liverpool");
